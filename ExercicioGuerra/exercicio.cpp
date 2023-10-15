710170d6c0d359fdd2a36fa3c171748e75fc521c
alexgab <alexgabriel3357@gmail.com>
2023-10-15 10:14:16 -0300

    adição de tratamento de entrada

--- ExercicioGuerra/exercicio.cpp
+++ ExercicioGuerra/exercicio.cpp
@@ -21,6 +21,47 @@ class Grafo {
 };
 
 int main(){
+  int vertices, arestas;
+  int array[500][500];
 
+  cin >> vertices;
+  cin >> arestas;
+
+  while(vertices != 0 || arestas != 0) {
+
+    Grafo cidade;
+
+    for(int i = 0; i < 500; i++) {
+      for(int j = 0; j < 500; j++) {
+        cidade.grafo[i][j] = -1;
+      }
+    };
+
+    int X, Y, custo;
+    int consultas;
+
+    for(int i = 0; i < arestas; i++) {
+      cin >> X;
+      cin >> Y;
+      cin >> custo;
+      cidade.insereAresta(X, Y, custo);
+    }
+
+    cin >> consultas;
+
+    for(int j = 0; j < consultas; j++){
+      cin >> X;
+      cin >> Y;
+
+      array[j][j] = X;
+      array[j+1][j+1] = Y;
+    }
+
+    cin >> vertices;
+    cin >> arestas;
+
+    cout << "\n";
+  }
+ 
   return 0;
 }
